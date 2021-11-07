66e54746913403e3858f0762ea1e3e66ea6fe3fe
Noa Farber <noaros88@gmail.com>
2021-11-07 14:05:16 +0000

    fixed encrypted password- now shows correctly in db

--- lib/user.rb
+++ lib/user.rb
@@ -16,7 +16,7 @@ class User
 
     result = DatabaseConnection.query(
       "INSERT INTO users (name, username, email, password) VALUES($1, $2, $3, $4)
-      RETURNING id, name, username, email;", [name, username, email, password]
+      RETURNING id, name, username, email;", [name, username, email, encrypted_password]
     )
     User.new(
       id: result[0]['id'], 
